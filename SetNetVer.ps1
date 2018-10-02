503968cafe0b0984e193f6590b038d745649b88d
Kurtis Cobb <kurtis.cobb@gmail.com>
2018-10-02 13:56:25 -0700

    Add files via upload

---
+++ SetNetVer.ps1
@@ -0,0 +1,15 @@
+﻿# Title: SetNetVer.ps1
+# Purpose: This script is used to detect the version dotnet in use and set variables accordingly
+
+$path1 = $(build.sourcesdirectory)\Medumo\Medumo.PatientDispositioner\bin\$(BuildConfiguration)
+
+if (Test-Path $path1\netstandard2.0\) {
+  Write-host "netstandard2.0 detected"
+  "##vso[task.setvariable variable=netVer;]netstandard2.o"
+} elseif (Test-Path $path1\net461\) {
+  Write-host "net461 detected"
+  "##vso[task.setvariable variable=netVer;]net461"
+} else {
+  Write-host "Dotnet version was not detected!"
+  exit
+}
